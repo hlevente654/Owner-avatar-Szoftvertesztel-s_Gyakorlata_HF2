70e8cdc2fec5f42fdc8c6dc4a38dd4f15d17b6df
hlevente654 <hlevente654@gmail.com>
2023-11-05 13:44:24 +0100

    Implement

--- src/example.ts
+++ src/example.ts
@@ -1,4 +1,55 @@
 export function hello(people: string[]): string {
-  
-  return "";
-}
+
+  if (people.length === 0) {
+    return 'Hello, my friend.';
+  }
+
+  const koszon: string[] = [];
+  const shoutingGreetings: string[] = [];
+
+  for (const person of people) {
+    if (person === person.toUpperCase()) {
+      shoutingGreetings.push(person);
+    } else {
+      koszon.push(person);
+    }
+  }
+  let greetingMessageNorm = 'Hello ';
+  if(koszon.length === 1 ){
+    greetingMessageNorm += koszon[0] + '.';
+  } else if (koszon.length > 1 ) {
+    for(var i = 0; i < koszon.length; i++){
+      if (i === koszon.length-1){
+        greetingMessageNorm = greetingMessageNorm.slice(0, -2) + ' ';
+        greetingMessageNorm += "and " + koszon[i] + ".";
+      } else{
+        greetingMessageNorm += koszon[i] + ", ";
+      }
+    }
+  }
+
+  let greetingMessageBold = 'Hello ';
+  if (shoutingGreetings.length === 1 ){
+    greetingMessageBold = "HELLO " + shoutingGreetings[0] + "!";
+  } else if (shoutingGreetings.length > 1 ) {
+    greetingMessageBold = 'HELLO ';
+    for(var i = 0; i < shoutingGreetings.length; i++){
+      if (i === shoutingGreetings.length-1){
+        greetingMessageBold = greetingMessageBold.slice(0, -2) + ' ';
+        greetingMessageBold += "AND " + shoutingGreetings[i] + "!";
+      } else{
+        greetingMessageBold += shoutingGreetings[i] + ", ";
+      }
+    }
+  }
+
+  if(shoutingGreetings.length === 0 && koszon.length > 0){
+    return greetingMessageNorm;
+  } else if(shoutingGreetings.length > 0 && koszon.length === 0){
+    return greetingMessageBold;
+  } else if(shoutingGreetings.length !== 0 && koszon.length !== 0){
+    return greetingMessageNorm + " " + greetingMessageBold;
+  } 
+
+    return "";
+  }
