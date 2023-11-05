f7186a0f8c4947c5677cb7ca489a8494d1d18f71
hlevente654 <hlevente654@gmail.com>
2023-11-05 13:45:20 +0100

    Refactor

--- src/example.ts
+++ src/example.ts
@@ -1,55 +1,35 @@
 export function hello(people: string[]): string {
-
   if (people.length === 0) {
     return 'Hello, my friend.';
   }
-
-  const koszon: string[] = [];
   const shoutingGreetings: string[] = [];
+  const normalGreetings: string[] = [];
 
   for (const person of people) {
     if (person === person.toUpperCase()) {
       shoutingGreetings.push(person);
     } else {
-      koszon.push(person);
-    }
-  }
-  let greetingMessageNorm = 'Hello ';
-  if(koszon.length === 1 ){
-    greetingMessageNorm += koszon[0] + '.';
-  } else if (koszon.length > 1 ) {
-    for(var i = 0; i < koszon.length; i++){
-      if (i === koszon.length-1){
-        greetingMessageNorm = greetingMessageNorm.slice(0, -2) + ' ';
-        greetingMessageNorm += "and " + koszon[i] + ".";
-      } else{
-        greetingMessageNorm += koszon[i] + ", ";
-      }
+      normalGreetings.push(person);
     }
   }
 
-  let greetingMessageBold = 'Hello ';
-  if (shoutingGreetings.length === 1 ){
-    greetingMessageBold = "HELLO " + shoutingGreetings[0] + "!";
-  } else if (shoutingGreetings.length > 1 ) {
-    greetingMessageBold = 'HELLO ';
-    for(var i = 0; i < shoutingGreetings.length; i++){
-      if (i === shoutingGreetings.length-1){
-        greetingMessageBold = greetingMessageBold.slice(0, -2) + ' ';
-        greetingMessageBold += "AND " + shoutingGreetings[i] + "!";
-      } else{
-        greetingMessageBold += shoutingGreetings[i] + ", ";
-      }
-    }
+  let normM = "";
+  if(normalGreetings.length <= 1){
+    normM = normalGreetings.join(', ');
+  }else {
+    const last = normalGreetings.pop();
+    normM = `${normalGreetings.join(', ')} and ${last}`;
   }
+  const normalGreeting = normalGreetings.length === 0 ? '' : `Hello ${normM}.`;
 
-  if(shoutingGreetings.length === 0 && koszon.length > 0){
-    return greetingMessageNorm;
-  } else if(shoutingGreetings.length > 0 && koszon.length === 0){
-    return greetingMessageBold;
-  } else if(shoutingGreetings.length !== 0 && koszon.length !== 0){
-    return greetingMessageNorm + " " + greetingMessageBold;
-  } 
-
-    return "";
+  let boldM = "";
+  if(shoutingGreetings.length <= 1){
+    boldM = shoutingGreetings.join(', ');
+  }else {
+    const last = shoutingGreetings.pop();
+    boldM = `${shoutingGreetings.join(', ')} AND ${last}`;
   }
+  const shoutingGreeting = shoutingGreetings.length === 0 ? '' : `HELLO ${boldM}!`;
+
+  return `${normalGreeting} ${shoutingGreeting}`.trim();
+}
